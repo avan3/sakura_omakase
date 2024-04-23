f907f02d9d92d97f9525d936fea178bdb1d768f9
Andrew <andrew.van1@live.com>
2024-04-23 14:10:24 -0400

    fixed navbar style and made background black

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,19 +5,21 @@ import { NavLinks, Link } from "../constants";
 const Navbar = () => {
   return (
     <nav
-      className={`w-full xl:w-5/6 flex justify-between items-center top-0 sm:px-16 px-6`}
+      className={`w-full xl:w-5/6 flex justify-between items-center top-0 sm:px-10 px-6`}
     >
       <img
         src={sakura_omakase_logo}
         alt="Sakura Omakase"
         className="w-[100px] h-[100px] my-2 relative"
       />
-      <span className="hidden sm:flex">
+      <span className="hidden sm:flex mt-2">
         <ul className="list-none flex">
-          {NavLinks.map((item: Link) => (
+          {NavLinks.map((item: Link, index: number) => (
             <li
               key={item.id}
-              className="font-fira_sans font-semibold uppercase cursor-pointer text-[12px] text-secondary hover:text-primary/95 mr-10"
+              className={`font-fira_sans font-semibold uppercase cursor-pointer 
+                text-[12px] text-secondary 
+                ${index < NavLinks.length - 1 ? "mr-10" : ""}`}
             >
               <a href={"#"}>{item.title}</a>
             </li>
