48ef25b83ebc79ed98b04d03d56d507e16360075
Andrew <andrew.van1@live.com>
2024-04-27 15:22:31 -0400

    added manualChunks in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { dependencies } from "./package.json";
+
+function renderChunks(deps: Record<string, string>) {
+  const chunks: Record<string, string[]> = {};
+  Object.keys(deps).forEach((key) => {
+    if (["react", "react-router-dom", "react-dom"].indexOf(key) !== -1) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-router-dom", "react-dom"],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+});
