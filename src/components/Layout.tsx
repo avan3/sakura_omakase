4cf31300d28fc5d68e8fd0a1103b43e63983ca91
Andrew <andrew.van1@live.com>
2024-04-27 15:26:58 -0400

    added margin x to layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,15 @@
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 
-const Layout = () => {
+export const Layout = () => {
   return (
     <div className="flex h-full">
       <Navbar />
       <div className="md:ml-[10%] sm:ml-[17%] xs:ml-[120px] ml-[100px]">
-        <div className="flex flex-col justify-center items-center text-black my-[80px] md:my-[110px] xl:my-[200px]">
+        <div className="flex flex-col justify-center items-center text-black my-[80px] md:my-[110px] xl:my-[200px] mx-3 sm:mx-10">
           <Outlet />
         </div>
       </div>
     </div>
   );
 };
-
-export default Layout;
