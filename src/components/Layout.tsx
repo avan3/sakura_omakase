8fe5e21c0fb22b76004385dff149387a17da73be
Andrew <andrew.van1@live.com>
2024-04-27 09:55:57 -0400

    updated layout to use flex

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = () => {
     <div className="flex h-full">
       <Navbar />
       <div className="md:ml-[10%] sm:ml-[17%] xs:ml-[120px] ml-[100px]">
-        <div className="text-black mt-[80px] md:mt-[110px] xl:mt-[200px]">
+        <div className="flex flex-col justify-center items-center text-black">
           <Outlet />
         </div>
       </div>
