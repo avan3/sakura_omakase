9ffd4463fa9e633a3212c7b0caf4630a14727088
Andrew <andrew.van1@live.com>
2024-04-27 15:25:56 -0400

    created welcome component

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,5 +1,8 @@
-const Welcome = () => {
-  return <div>Welcome</div>;
+export const Welcome = () => {
+  return (
+    <section className="flex flex-col md:flex-row w-full mt-4 md:mt-6">
+      <section className="md:basis-1/2">Welcome</section>
+      <section className="md:basis-1/2">Another Image</section>
+    </section>
+  );
 };
-
-export default Welcome;
