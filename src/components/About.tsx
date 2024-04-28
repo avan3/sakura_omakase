4c53178b86d3e72285139460ae94ca5d5d8caf42
Andrew <andrew.van1@live.com>
2024-04-28 14:47:32 -0400

    finished about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,36 +1,61 @@
 export const About = () => {
   return (
-    <>
-      Welcome to Sakura Omakase, where culinary artistry meets Japanese
-      tradition in every dish. Nestled in the heart of Toronto, we invite you on
-      a journey of exquisite flavors and unparalleled dining experiences.
-      <br />
-      At Sakura Omakase, we specialize in the time-honored tradition of omakase,
-      where our skilled chefs curate an unforgettable meal tailored to your
-      palate. Each course is a masterpiece, meticulously crafted using the
-      finest ingredients sourced locally and from Japan to ensure authenticity
-      and quality.
-      <br />
-      Our restaurant is named after the iconic sakura, or cherry blossom,
-      symbolizing the ephemeral beauty and fleeting nature of life—a sentiment
-      echoed in our culinary creations. Just as the sakura blooms in delicate
-      splendor for a brief moment, each dish at Sakura Omakase is a fleeting
-      delight, meant to be savored and cherished.
-      <br />
-      Step into our elegant dining space and immerse yourself in the tranquil
-      ambiance of Japan. From the soothing sounds of flowing water to the
-      minimalist décor inspired by Japanese aesthetics, every detail has been
-      thoughtfully curated to enhance your dining experience.
-      <br />
-      Whether you're a seasoned connoisseur or new to the world of omakase, our
-      knowledgeable staff is dedicated to providing exceptional service, guiding
-      you through each course with warmth and expertise.
-      <br />
-      Join us at Sakura Omakase and embark on a culinary journey like no other.
-      Let us transport you to the heart of Japan, where every bite tells a story
-      and every meal is a celebration of artistry and tradition.
-      <br />
-      Indulge in the elegance of omakase dining. Indulge in Sakura Omakase.
-    </>
+    <section className="flex md:my-6 justify-center items-center overflow-hidden relative">
+      <section
+        className="p-10 bg-cover bg-no-repeat brightness-40 absolute rounded-sm 
+      w-full md:w-2/3 h-full justify-center items-center flex
+      bg-sushi_xs xs:bg-sushi_ss ss:bg-sushi_sm sm:bg-sushi_md
+      md:bg-sushi_lg lg:bg-sushi_xl xl:bg-sushi_xxl xxl:bg-sushi_xxxxl"
+      ></section>
+      <section className="z-10 p-10 text-white md:basis-2/3 text-left font-fira_sans md:text-lg lg:text-xl xl:text-3xl xxxl:text-4xl xxxxl:text-5xl">
+        <h3 className="font-semibold text-[2.5rem] text-center">About</h3>
+        <p className="mt-8 text-md sm:text-lg md:text-xl xl:text-3xl xxxl:text-4xl xxxxl:text-5xl">
+          <span className="font-semibold text-lg sm:text-xl md:text-2xl xl:text-4xl xxxl:text-5xl xxxxl:text-6xl">
+            Welcome to Sakura Omakase
+          </span>
+          , where culinary artistry meets Japanese tradition in every dish.
+          Nestled in the heart of Toronto, we invite you on a journey of
+          exquisite flavors and unparalleled dining experiences.
+        </p>
+        <p className="mt-4 text-md sm:text-lg md:text-xl xl:text-3xl xxxl:text-4xl xxxxl:text-5xl">
+          At Sakura Omakase, we specialize in the time-honored tradition of
+          omakase, where our skilled chefs curate an unforgettable meal tailored
+          to your palate. Each course is a masterpiece, meticulously crafted
+          using the finest ingredients sourced locally and from Japan to ensure
+          authenticity and quality.
+        </p>
+        <p className="mt-4 text-md sm:text-lg md:text-xl xl:text-3xl xxxl:text-4xl xxxxl:text-5xl">
+          Our restaurant is named after the iconic sakura, or cherry blossom,
+          symbolizing the ephemeral beauty and fleeting nature of life—a
+          sentiment echoed in our culinary creations. Just as the sakura blooms
+          in delicate splendor for a brief moment, each dish at Sakura Omakase
+          is a fleeting delight, meant to be savored and cherished.
+        </p>
+        <p className="mt-4 text-md sm:text-lg md:text-xl xl:text-3xl xxxl:text-4xl xxxxl:text-5xl">
+          Step into our elegant dining space and immerse yourself in the
+          tranquil ambiance of Japan. From the soothing sounds of flowing water
+          to the minimalist décor inspired by Japanese aesthetics, every detail
+          has been thoughtfully curated to enhance your dining experience.
+        </p>
+        <p className="mt-4 text-md sm:text-lg md:text-xl xl:text-3xl xxxl:text-4xl xxxxl:text-5xl">
+          Whether you're a seasoned connoisseur or new to the world of omakase,
+          our knowledgeable staff is dedicated to providing exceptional service,
+          guiding you through each course with warmth and expertise.
+        </p>
+        <p className="mt-4 text-md sm:text-lg md:text-xl xl:text-3xl xxxl:text-4xl xxxxl:text-5xl">
+          Join us at Sakura Omakase and embark on a culinary journey like no
+          other. Let us transport you to the heart of Japan, where every bite
+          tells a story and every meal is a celebration of artistry and
+          tradition.
+        </p>
+        <p className="mt-4 text-left text-md sm:text-lg md:text-xl xl:text-3xl xxxl:text-4xl xxxxl:text-5xl">
+          Indulge in the elegance of omakase dining.
+          <br />
+          <span className="font-semibold text-lg sm:text-xl md:text-2xl xl:text-4xl xxxl:text-5xl xxxxl:text-6xl">
+            Indulge in Sakura Omakase.
+          </span>
+        </p>
+      </section>
+    </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,19 @@ export default {
       fontFamily: {
         fira_sans: ["Fira Sans Extra Condensed", "sans-serif"],
       },
+      backgroundImage: {
+        sushi_xs: "url('/src/assets/sushi-bg_480x719.webp')",
+        sushi_ss: "url('/src/assets/sushi-bg_620x929.webp')",
+        sushi_sm: "url('/src/assets/sushi-bg_768x1151.webp')",
+        sushi_md: "url('/src/assets/sushi-bg_1060x1588.webp')",
+        sushi_lg: "url('/src/assets/sushi-bg_1200x1798.webp')",
+        sushi_xl: "url('/src/assets/sushi-bg_1700x2547.webp')",
+        sushi_xxl: "url('/src/assets/sushi-bg_2000x2997.webp')",
+        sushi_xxxxl: "url('/src/assets/sushi-bg_2636x3950.webp')",
+      },
+      brightness: {
+        40: ".40",
+      },
     },
     screens: {
       xs: "480px",
