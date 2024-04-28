d25d9a6490adbecb61d4c5270575448db9a44aa1
Andrew <andrew.van1@live.com>
2024-04-28 15:24:05 -0400

    update footer starting with horizontal line

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,16 @@
-const Footer = () => {
-  return <div>Footer</div>;
+export const Footer = () => {
+  return (
+    <>
+      <div className="relative py-4 my-4 w-full">
+        <div className="absolute inset-0 flex items-center">
+          <div className="w-full border-b border-black"></div>
+        </div>
+        {/* <div className="relative flex justify-center">
+          <span className="bg-white px-4 font-medium text-md md:text-lg lg:text-xl xl:text-2xl xxxl:text-3xl xxxxl:text-4xl text-black">
+            Continue
+          </span>
+        </div> */}
+      </div>
+    </>
+  );
 };
-
-export default Footer;
