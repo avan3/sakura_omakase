6357f5a98e0491e366e9676ef5a0b231054289bc
Andrew <andrew.van1@live.com>
2024-04-27 21:24:55 -0400

    updated cta component

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,5 +1,12 @@
-import React from "react";
-
 export const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section className="flex flex-col md:flex-row md:my-6 justify-center items-center">
+      <section className="my-3 mx-4">
+        <h3 className="font-fira_sans font-semibold text-[2.5rem]">Menu</h3>
+        <button className="">Menu</button>
+      </section>
+      {/* <section className="md:basis-1/2 my-3 mx-4">2</section>
+      <section className="md:basis-1/2 my-3 mx-4">3</section> */}
+    </section>
+  );
 };
