83e1638371e685b542ffe8ac489f1d757e387528
Andrew <andrew.van1@live.com>
2024-04-29 08:03:45 -0400

    updated reservations section

--- src/components/Reservations.tsx
+++ src/components/Reservations.tsx
@@ -1,3 +1,11 @@
 export const Reservations = () => {
-  return <div>Reservations</div>;
+  return (
+    <section className="flex flex-col md:flex-row md:my-6 justify-center items-center">
+      <section className="md:basis-2/3 my-3 mx-4 text-left font-fira_sans md:text-lg lg:text-xl xl:text-3xl xxxl:text-4xl xxxxl:text-5xl">
+        <h3 className="font-semibold text-[48px] xs:text-[50px] sm:text-[64px] lg:text-[76px] text-center">
+          Reservations
+        </h3>
+      </section>
+    </section>
+  );
 };
