6d3d5341fac4d0f9bd01d9f89930930dd855e608
Andrew <andrew.van1@live.com>
2024-04-28 22:34:14 -0400

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,19 @@
 export const Footer = () => {
   return (
-    <>
-      <div className="relative py-4 my-4 w-full">
+    <div className="w-full my-4 xl:py-5 xl:my-5 xxl:py-6 xxl:my-6 xxxl:py-7 xxxl:my-7 xxxxl:py-8 xxxxl:my-8">
+      <div className="relative py-4">
         <div className="absolute inset-0 flex items-center">
           <div className="w-full border-b border-black"></div>
         </div>
         {/* <div className="relative flex justify-center">
           <span className="bg-white px-4 font-medium text-md md:text-lg lg:text-xl xl:text-2xl xxxl:text-3xl xxxxl:text-4xl text-black">
-            Continue
+          Continue
           </span>
         </div> */}
       </div>
-    </>
+      <div className="relative font-fira_sans text-md xl:text-lg xxl:text-xl xxxl:text-2xl xxxxl:text-3xl">
+        ©Copyright Sakura Omakase 2024
+      </div>
+    </div>
   );
 };
