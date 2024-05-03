e13c2ed4b4db6b45312895c3193ab2508b1252fb
Andrew <andrew.van1@live.com>
2024-05-03 06:37:43 -0400

    added fontawesome package for chevron icons in input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,9 @@
 import { InputHTMLAttributes, ReactNode } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   children?: ReactNode;
@@ -6,9 +11,21 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export const Input = ({ name, children }: InputProps) => {
   return (
-    <div className="basis-1/3 w-full">
-      {name}
-      {children}
+    <div className="basis-1/3 w-full flex justify-center items-center">
+      <div className="basis-2/3 flex justify-center items-center">
+        <FontAwesomeIcon
+          icon={faChevronLeft}
+          className="w-5 h-5 xl:w-6 xl:h-6 xxl:w-7 xxl:h-7 xxxl:w-8 xxxl:h-8"
+        />
+        <div className="flex flex-col justify-center items-center">
+          {name}
+          {children}
+        </div>
+        <FontAwesomeIcon
+          icon={faChevronRight}
+          className="w-5 h-5 xl:w-6 xl:h-6 xxl:w-7 xxl:h-7 xxxl:w-8 xxxl:h-8"
+        />
+      </div>
     </div>
   );
 };
