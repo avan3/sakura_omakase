439605dbad3c9642e913cbb562bfab9dff5d8b85
Andrew <andrew.van1@live.com>
2024-05-08 16:25:32 -0400

    moved logic for initial reservation search from form component to step1 component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,8 @@
-import { useCallback, useState, useRef } from "react";
-import { Input } from "./Input";
-import { useForm } from "react-hook-form";
+import { useState, useRef } from "react";
+import { Step1 } from "./Step1";
+import { Step2 } from "./Step2";
 
 export const Form = () => {
-  const { register, watch, setValue } = useForm();
   const [formStep, setFormStep] = useState<number>(0);
   const round = 1000 * 60 * 15;
   const startDateTime = new Date(new Date().setHours(17, 0, 0, 0));
@@ -15,48 +14,6 @@ export const Form = () => {
   );
   const [guests, setGuests] = useState<number>(1);
 
-  // Increment or decrement date
-  const incrementDate = useCallback(() => {
-    setDate(addDays(date, 1));
-  }, [date]);
-
-  const decrementDate = useCallback(() => {
-    setDate(addDays(date, -1));
-  }, [date]);
-
-  function addDays(date: Date, number: number) {
-    if (date.getDate() < currentDateTime.current.getDate()) {
-      return date;
-    }
-    const newDate = new Date(date);
-    return new Date(newDate.setDate(newDate.getDate() + number));
-  }
-
-  // Increment or decrement time
-  const incrementTime = useCallback(() => {
-    setDate(addTime(date, 15));
-  }, [date]);
-
-  const decrementTime = useCallback(() => {
-    setDate(addTime(date, -15));
-  }, [date]);
-
-  function addTime(date: Date, number: number) {
-    if (date.getTime() < currentDateTime.current.getTime()) {
-      return date;
-    }
-    const newDate = new Date(date);
-    return new Date(newDate.setMinutes(newDate.getMinutes() + number));
-  }
-
-  // Increment or decrement guests
-  function addGuests(guests: number, number: number) {
-    if (guests < 1) {
-      return guests;
-    }
-    return guests + number;
-  }
-
   return (
     <form noValidate onSubmit={(e) => e.preventDefault()}>
       <div
@@ -64,88 +21,19 @@ export const Form = () => {
         text-center divide-y divide-black border-t border-b border-black"
       >
         {formStep == 0 && (
-          <>
-            {/* Increment or decrement by 1 day */}
-            <Input
-              name="Date"
-              increment={incrementDate}
-              decrement={decrementDate}
-              disableDecrement={
-                date.getDate() <= currentDateTime.current.getDate()
-              }
-            >
-              <div>{`${date.toLocaleString("en-CA", {
-                weekday: "short",
-                month: "short",
-                day: "numeric",
-              })}`}</div>
-            </Input>
-            {/* Increment or decrement by 15 minutes */}
-            <Input
-              name="Time"
-              increment={incrementTime}
-              decrement={decrementTime}
-              disableDecrement={
-                date.getTime() <= currentDateTime.current.getTime()
-              }
-            >
-              {`${date.toLocaleString("en-CA", {
-                hour: "numeric",
-                minute: "numeric",
-              })}`}
-            </Input>
-            {/* Increment or decrement by 1 guest */}
-            <Input
-              name="Guest"
-              increment={() => setGuests((prev) => addGuests(prev, 1))}
-              decrement={() => setGuests((prev) => addGuests(prev, -1))}
-              disableDecrement={guests <= 1}
-            >
-              {guests}
-            </Input>
-            <div className="basis-2/3 w-full flex justify-center items-center">
-              <div className="flex justify-center items-center flex-1">
-                <button
-                  className={`font-fira_sans font-medium bg-black hover:opacity-25
-                cursor-pointer text-[17px] xl:text-[21px] xxl:text-[25px] xxxl:text-[30px] 
-                text-secondary w-2/3 p-1`}
-                  type="button"
-                  onClick={() => setFormStep((prev) => prev + 1)}
-                >
-                  Search
-                </button>
-              </div>
-            </div>
-            {/* <input
-              type="date"
-              value={`${date.toLocaleString("en-CA", {
-                year: "numeric",
-                month: "2-digit",
-                day: "2-digit",
-              })}`}
-              {...register("date", { required: true })}
-              onChange={(e) => setValue("date", e.target.value)}
-            />
-            <input
-              type="time"
-              value={`${date.toLocaleTimeString("en-CA", {
-                hour12: false,
-              })}`}
-              {...register("time", { required: true })}
-              onChange={(e) => setValue("time", e.target.value)}
-            />
-            <input
-              type="number"
-              hidden
-              aria-hidden
-              value={guests}
-              {...register("guests", { required: true })}
-              onChange={(e) => setValue("guests", e.target.value)}
-            /> */}
-          </>
+          <Step1
+            currentDateTime={currentDateTime.current}
+            date={date}
+            setDate={setDate}
+            guests={guests}
+            setGuests={setGuests}
+            setFormStep={setFormStep}
+          />
+        )}
+        {formStep == 1 && (
+          <Step2 date={date} guests={guests} setFormStep={setFormStep} />
         )}
       </div>
-      <pre>{JSON.stringify(watch(), null, 2)}</pre>
     </form>
   );
 };
---
+++ src/components/Step1.tsx
@@ -0,0 +1,152 @@
+import { Dispatch, SetStateAction, useCallback } from "react";
+import { Input } from "./Input";
+import { useForm } from "react-hook-form";
+
+type Step1Props = {
+  currentDateTime: Date;
+  date: Date;
+  setDate: Dispatch<SetStateAction<Date>>;
+  guests: number;
+  setGuests: Dispatch<SetStateAction<number>>;
+  setFormStep: Dispatch<SetStateAction<number>>;
+};
+
+export const Step1 = ({
+  currentDateTime,
+  date,
+  setDate,
+  guests,
+  setGuests,
+  setFormStep,
+}: Step1Props) => {
+  const { watch, setValue } = useForm({ defaultValues: { date, guests } });
+
+  // Increment or decrement date
+  const addDays = useCallback(
+    (date: Date, number: number) => {
+      if (date.getDate() < currentDateTime.getDate()) {
+        return currentDateTime;
+      }
+      const newDate = new Date(date);
+      return new Date(newDate.setDate(newDate.getDate() + number));
+    },
+    [currentDateTime]
+  );
+
+  const incrementDate = useCallback(() => {
+    setDate((prev) => {
+      const newDate = addDays(prev, 1);
+      setValue("date", newDate);
+      return newDate;
+    });
+  }, [addDays, setValue, setDate]);
+
+  const decrementDate = useCallback(() => {
+    setDate((prev) => {
+      const newDate = addDays(prev, -1);
+      setValue("date", newDate);
+      return newDate;
+    });
+  }, [addDays, setValue, setDate]);
+
+  // Increment or decrement time
+  const addTime = useCallback(
+    (date: Date, number: number) => {
+      if (date.getTime() < currentDateTime.getTime()) {
+        return currentDateTime;
+      }
+      const newDate = new Date(date);
+      return new Date(newDate.setMinutes(newDate.getMinutes() + number));
+    },
+    [currentDateTime]
+  );
+
+  const incrementTime = useCallback(() => {
+    setDate((prev) => {
+      const newDate = addTime(prev, 15);
+      setValue("date", newDate);
+      return newDate;
+    });
+  }, [addTime, setValue, setDate]);
+
+  const decrementTime = useCallback(() => {
+    setDate((prev) => {
+      const newDate = addTime(prev, -15);
+      setValue("date", newDate);
+      return newDate;
+    });
+  }, [addTime, setValue, setDate]);
+
+  // Increment or decrement guests
+  function addGuests(guests: number, number: number) {
+    if (guests < 1) {
+      return guests;
+    }
+    return guests + number;
+  }
+  return (
+    <>
+      <pre>{JSON.stringify(watch(), null, 2)}</pre>
+      {/* Increment or decrement by 1 day */}
+      <Input
+        increment={incrementDate}
+        decrement={decrementDate}
+        disableDecrement={date.getDate() <= currentDateTime.getDate()}
+      >
+        <div>{`${date.toLocaleString("en-CA", {
+          weekday: "short",
+          month: "short",
+          day: "numeric",
+        })}`}</div>
+        <div className="opacity-25">Date</div>
+      </Input>
+      {/* Increment or decrement by 15 minutes */}
+      <Input
+        increment={incrementTime}
+        decrement={decrementTime}
+        disableDecrement={date.getTime() <= currentDateTime.getTime()}
+      >
+        {`${date.toLocaleString("en-CA", {
+          hour: "numeric",
+          minute: "numeric",
+        })}`}
+        <div className="opacity-25">Time</div>
+      </Input>
+      {/* Increment or decrement by 1 guest */}
+      <Input
+        name="guest"
+        increment={() =>
+          setGuests((prev) => {
+            const newGuests = addGuests(prev, 1);
+            setValue("guests", newGuests);
+            return newGuests;
+          })
+        }
+        decrement={() =>
+          setGuests((prev) => {
+            const newGuests = addGuests(prev, -1);
+            setValue("guests", newGuests);
+            return newGuests;
+          })
+        }
+        disableDecrement={guests <= 1}
+      >
+        {guests}
+        <div className="opacity-25">{guests > 1 ? "Guests" : "Guest"}</div>
+      </Input>
+      <div className="basis-2/3 w-full flex justify-center items-center">
+        <div className="flex justify-center items-center flex-1">
+          <button
+            className={`font-fira_sans font-medium bg-black hover:opacity-25
+                cursor-pointer text-[17px] xl:text-[21px] xxl:text-[25px] xxxl:text-[30px] 
+                text-secondary w-2/3 p-1`}
+            type="button"
+            onClick={() => setFormStep((prev) => prev + 1)}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
