856bf6b734383be93361c62d381f7bed2d90eaf2
Andrew <andrew.van1@live.com>
2024-05-13 06:55:41 -0400

    updated width of step2 and hover behaviour

--- src/components/Step2.tsx
+++ src/components/Step2.tsx
@@ -10,7 +10,11 @@ type Step2Props = {
 
 const generateNextOptions = () => {
   // TODO: generate options for the next 15 minute interval randomly
-  const random = Math.random();
+  const max = 8;
+  const min = 4;
+  const round = 1000 * 60 * 15;
+  const random = Math.floor(Math.random() * (max - min + 1) + min);
+  console.log("random: " + random);
   return [];
 };
 
@@ -18,7 +22,7 @@ const OPTIONS = generateNextOptions();
 
 export const Step2 = ({ date, guests, setFormStep }: Step2Props) => {
   return (
-    <div className="w-full max-w-[500px] text-white my-4">
+    <div className="w-full max-w-[90%] text-white my-4">
       <div className="py-3 bg-[#00b496] min-h-[40px]">
         <div className="float-left px-3">
           <button type="button" onClick={() => setFormStep((prev) => prev - 1)}>
@@ -48,33 +52,39 @@ export const Step2 = ({ date, guests, setFormStep }: Step2Props) => {
           Select a time at Sakura Omakase
         </p>
         <div className="py-3 grid grid-cols-2 justify-between w-[95%] m-auto">
-          {OPTIONS.map((option) => (
-            <button>asdf</button>
+          {OPTIONS.map(({ time, description }) => (
+            <button
+              type="button"
+              className="text-center bg-[#34675d] m-2 hover:text-slate-300 rounded-sm text-[18px] leading-5 py-2"
+            >
+              <p>{time}</p>
+              <p>{description}</p>
+            </button>
           ))}
           <button
             type="button"
-            className="text-center bg-[#34675d] m-2 hover:text-slate-300 rounded-sm text-[18px] leading-5 py-2"
+            className="text-center bg-[#34675d] m-2 hover:brightness-90 rounded-sm text-[18px] leading-5 py-2"
           >
             <p>5:00pm</p>
             <p>À La Carte</p>
           </button>
           <button
             type="button"
-            className="text-center bg-[#34675d] m-2 hover:text-slate-300 rounded-sm text-[18px] leading-5 py-2"
+            className="text-center bg-[#34675d] m-2 hover:brightness-90 rounded-sm text-[18px] leading-5 py-2"
           >
             <p>5:15pm</p>
             <p>À La Carte</p>
           </button>
           <button
             type="button"
-            className="text-center bg-[#34675d] m-2 hover:text-slate-300 rounded-sm text-[18px] leading-5 py-2"
+            className="text-center bg-[#34675d] m-2 hover:brightness-90 rounded-sm text-[18px] leading-5 py-2"
           >
             <p>5:30pm</p>
             <p>À La Carte</p>
           </button>
           <button
             type="button"
-            className="text-center bg-[#34675d] m-2 hover:text-slate-300 rounded-sm text-[18px] leading-5 py-2"
+            className="text-center bg-[#34675d] m-2 hover:brightness-90 rounded-sm text-[18px] leading-5 py-2"
           >
             <p>5:45pm</p>
             <p>À La Carte</p>
